3a1180703d91236ad23021412be8caff17444e78
Allan Stokes <allan@stokes.ca>
2012-11-09 02:11:23 -0800

    test program QR_here.py generates a dynamic QR image of current URL

--- etude/QR_here.py
+++ etude/QR_here.py
@@ -1,14 +1,45 @@
 # QR_here.py displays the current URL encoded as a QR image.  
+
+import os, sys, inspect
+
+#
+# Much ugly bother to work with a local copy of pyqrcode
+#
+curpath = os.path.split(inspect.getfile(inspect.currentframe()))[0]
+newpath = os.path.join(curpath, "../pyqrcode-read-only")
+cmd_folder = os.path.realpath(os.path.abspath(newpath))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+try:
+    import pyqrcode
+except ImportError:
+    print "you need to run the script to obtain the pyqrcode module"
+    sys.exit (1)
  
 import bottle
 import pymongo
+import StringIO
 
 print "binding to wildcard route at http://yourhost/"
-@bottle.route('<:re:.*>')   # all-route regex wildcard
-def index(): 
-	from pymongo import Connection
-	connection = Connection('localhost', 27017)
-	return "<html><body>Hello, %s!</body></html>" % item['name']
+@bottle.route('<path:re:.*>')   # all-route regex wildcard
+def index(path): 
+    # path does not contain scheme, hostname, or port 
+    route_URL = 'http://' + path 
+
+    URL = bottle.request.url 
+
+    from pymongo import Connection
+    connection = Connection('localhost', 27017)
+    # pyqrcode bails out if URL string is shorter than http:/
+    img = pyqrcode.MakeQRImage(URL, rounding = 0, fg = "black", bg = "burlywood", br = False)
+
+    bottle.response.set_header('Content-type', 'image/png')
+
+    img_buff = StringIO.StringIO()
+    img.save(img_buff, format='png')
+    img_buff.seek(0)
+
+    return img_buff.read() 
 
 bottle.run(host='0.0.0.0', port=8800)
 
--- etude/generate_QR_image.py
+++ etude/generate_QR_image.py
@@ -1,9 +1,35 @@
-import pyqrcode
+# Simple program to call pyqrcode to generate a URL based QR code image 
+# and save the generated image to disk 
 
-URL = "http://www.discoveranywheremobile.com/"
-URL = "http://m.visitpalmsprings.com/listings/x-1fe89fc957e563c7/x-08cdb36014211c43/l-22c2cdd72d95ff48/"
-URL = 'http://wine.stokes.ca/12W1/23'
+# The generated QR image was tested with a Samsung Galaxy II running the 
+# Norton Snap QR Code Reader from the Android Play Store 
+
+import os, sys, inspect
+
+# 
+# Much ugly bother to work with a local copy of pyqrcode 
+#
+curpath = os.path.split(inspect.getfile(inspect.currentframe()))[0]
+newpath = os.path.join(curpath, "../pyqrcode-read-only")
+cmd_folder = os.path.realpath(os.path.abspath(newpath))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+try:
+    import pyqrcode
+except ImportError:
+    print "you need to run the script to obtain the pyqrcode module"
+    sys.exit (1) 
+
+
+
+URL = 'https://education.10gen.com/courses/10gen/M101/2012_Fall/'
+image = 'image/generate_QR_image.png'
 
 qr_image = pyqrcode.MakeQRImage(URL, rounding = 0, fg = "black", bg = "burlywood", br = False)
-qr_image.save('test.png')
-qr_image.show()
+qr_image.save(image)
+
+print "Test QR image saved to", image
+
+# the following command runs ImageMagick on my system to display the image in a Window
+# qr_image.show()
+
