f651cf5c3e0264b52a697ccacb9860ab86fda06e
Allan Stokes <allan@stokes.ca>
2012-11-11 00:24:06 -0800

    form cookie redirect technique added

--- etude/form_elements.py
+++ etude/form_elements.py
@@ -27,14 +27,33 @@ def password_form():
 print "binding to http://yourhost/radiobutton"
 @bottle.post('/radiobutton')
 def radio_form(): 
-	S = request.forms.sex
-	pronoun = ['his', 'her'][S=='female']
+	# contains logic to prove we remember cookie elsewhere 
+	G = request.forms.sex
+	S = request.get_cookie('purience', G)
+	pronoun = ['his', 'her'][G=='female']
 	return "<html><body>The human %s signals %s desires indirectly.</body></html>" % ( 
           S, pronoun)
 
+# patterned after WEEK1 Bottle framework: using cooki" %" % P
+# eliminates problem with user unable to refresh POST requests
+# CONTROLLER side
+print "binding to http://yourhost/radcook"
+@bottle.post('/radcook')
+def radcook_form(): 
+	P = request.forms.purience
+	bottle.response.set_cookie('purience',P)
+	bottle.redirect('/show_radcook')
+# 
+# VIEW side
+@bottle.route('/show_radcook')
+def show_radcook(): 
+	P = request.get_cookie('purience') 
+	return "<html><body>You're a %s, and we'll not forget it!</body></html>" % P
+
+
 print "binding to http://yourhost/checkbox"
 @bottle.post('/checkbox')
-def radio_form(): 
+def checkbox_form(): 
 	OS = request.forms.getall('phoneOS')
 	OS.insert (0, 'Your landline')  
 	# deprecate everything, picking on the last item only 
--- etude/views/form_elements.tpl
+++ etude/views/form_elements.tpl
@@ -35,14 +35,22 @@ For security and simplicity password must consist of symbols only<br/>
 </form>
 <br/>
 
+<form action="/radcook" method="POST" enctype="multipart/form-data"> 
+<input type="radio" name="purience" value="prude"/>Prude<br/>
+<input type="radio" name="purience" value="pervert"/>Pervert<br/>
+<input type="submit" value="Submit"/>
+</form>
+<br/>
+
 <form action="/checkbox" method="POST" enctype="multipart/form-data"> 
-<!-- the reference shows no default selection -->
 <input type="checkbox" name="phoneOS" value="Android"/>I own an Android phone<br/>
 <input type="checkbox" name="phoneOS" value="iOS"/>I own an iOS phone<br/>
 <input type="checkbox" name="phoneOS" value="Blackberry"/>I once owned a Blackberry phone<br/>
 <input type="checkbox" name="phoneOS" value="Symbian"/>I once owned a Symbian phone<br/>
 <input type="submit" value="Submit"/>
 </form>
+</br>
+
 
 <!-- TODO
 -- figure out <input type='checkbox' name='lang[en]'/>
